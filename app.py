8976fc5572718d801e8b006e3fbadb136c15ee80
RCY-FUDAN <rcy-fudan@ioeb-FUDANdeMacBook-Pro.local>
2017-12-24 15:49:23 +0800

    allok

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
+
+
+#reference url https://scotch.io/tutorials/build-a-crud-web-app-with-python-and-flask-part-one
+#when u run app.py in macos ,u may meet the problem https://stackoverflow.com/questions/24001147/python-bind-socket-error-errno-13-permission-denied
 import os,time,json
 #import datetime
+from dateutil import parser
 from datetime import datetime
-from flask import Flask, request,jsonify
-from flask import render_template
+from flask import Flask, request,jsonify,redirect, url_for
+from flask import render_template,make_response
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 #in procfile to specify the python run the app
 #web: FLASK_APP = server.py python -m flask run --host=0.0.0.0 --port=$PORT
 app = Flask(__name__)
+#i want to use other code ,but i donot have tes env,besides aliyun server seem like to be allow to be visited!!
+#use the command maybe slove the above problem,GRANT ALL PRIVILEGES ON dreamteam_db . * TO 'dt_admin'@'localhost';
+#the link is https://scotch.io/tutorials/build-a-crud-web-app-with-python-and-flask-part-one
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dt_admin:dt2016@localhost/dreamteam_db'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
+
+
+
+
+
+
+#db migrate ,u will meet the problem  You did not provide the FLASK_APP environment variable.ref:https://www.pythonanywhere.com/forums/topic/11946/
+#https://stackoverflow.com/questions/32798937/cant-migrate-or-upgrade-database-with-flask-migrate-alembic
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(80))
+    cookie = db.Column(db.String(500))
+    registerday = db.Column(db.String(80))
 
-    def __init__(self, name, email):
+    def __init__(self, name, email,password,cookie):
         self.name = name
         self.email = email
+        self.password = password
+        self.cookie = cookie
 
     def __repr__(self):
         return '<Name %r>' % self.name
@@ -46,10 +68,17 @@ class task(db.Model):
         self.project = project
         self.plantime = plantime
         self.finishtime = finishtime
-
-
     def __repr__(self):
         return '<Name %r>' % self.name
+
+
+
+
+
+
+
+
+
 
 #crete table!
 db.create_all()
@@ -157,12 +186,34 @@ def load():
 
 @app.route('/inbox')
 def inbox():
+    #i want to write it to a fcuntion!!!!
+    #get cookie key and value
+    useremail = request.cookies.get('email')
+    dbuser = User.query.filter_by(email=useremail).first()
+    print(dbuser.cookie)
+    if checkcookie_time(request)>10:
+        return redirect('/')
+    #https://teamtreehouse.com/community/flask-redirect-vs-redirecturlfor
+    if dbuser.cookie !=useremail:
+        return redirect('/')
     allinbox = task.query.filter_by(project='inbox').order_by(task.id).all()
     for k in allinbox:
         print(k.task)
     return render_template("inbox.html",my_list = allinbox)
 
 
+
+
+def checkcookie_time(request):
+    useremail = request.cookies.get('email')
+    logintime = request.cookies.get('logintime')
+    #i use the https://stackoverflow.com/questions/466345/converting-string-into-datetime to parser time string
+    logintime = parser.parse(logintime)
+    now = datetime.now()
+    #comupte interval senconds
+    #https://stackoverflow.com/questions/4362491/how-do-i-check-the-difference-in-seconds-between-two-dates
+    allseconds = (now-logintime).total_seconds()
+    return allseconds
 
 
 
@@ -345,6 +396,78 @@ def information1():
         return jsonify(input1=str("yangmingis=====here"),info1 =str(info))
 
 
+
+
+
+@app.route("/register",methods=['GET','POST'])
+def signup1():
+    username=request.form['username']
+    password=request.form['password']
+    email = request.form['email']
+    #ef __init__(self, name, email,password,cookie):
+    user = User(username,email,password,"nocookie")
+    db.session.add(user)
+    db.session.commit() 
+    #return json.dumps({"username":username,"password":password})
+    return render_template('user.html')
+ 
+'''
+            target_task = task.query.filter_by(id=idcli).first()
+            target_task.project = project
+            target_task.plantime = plantime
+            target_task.finishtime = finishtime
+            target_task.status =taskstatus
+'''
+
+
+#i design the user tem use cookie ,when the now -user cookie login time>100s
+#u need to relogin,i need to rethink db cookie where i can use??? 
+
+
+#i using the link to set cookie
+#http://junxiandoc.readthedocs.io/en/latest/docs/flask/flask_cookie_session.html
+@app.route("/login",methods=['GET','POST'])
+def login():
+    email=request.form['email']
+    password=request.form['password']
+    user = User.query.filter_by(email=email).first()
+    if user.password == password:
+        logininfo = "login successfully"
+    #resp = make_response(render_template('inbox.html'))
+    #url redirect https://stackoverflow.com/questions/14343812/redirecting-to-url-in-flask
+    resp = make_response(redirect('/inbox'))
+    resp.set_cookie('username', user.name)
+    resp.set_cookie('email', user.email)
+    expire_date = datetime.now()
+    '''
+    expire_date = expire_date + datetime.timedelta(seconds=20)
+    print(expire_date)
+    resp.set_cookie('email', user.email,expires=expire_date)
+    '''
+    resp.set_cookie('email', user.email)
+    #resp.set_cookie('Path','/inbox')
+    resp.set_cookie('test','yangming')
+    resp.set_cookie('test1','yangming1')
+    resp.set_cookie('logintime',str(datetime.now()))
+    #resp.set_cookie('expires',expire_date)
+    #set cookie outdated time
+    user.cookie=str(email)
+    db.session.commit()
+    #return json.dumps({"email":email,"password":password,"info":logininfo}) 
+    return resp
+
+
+
+#i using the link to set cookie
+#http://junxiandoc.readthedocs.io/en/latest/docs/flask/flask_cookie_session.html
+
+@app.route("/testcookie",methods=['GET','POST'])
+def cookie():
+    resp = make_response(render_template('inbox.html'))
+    resp.set_cookie('username', 'theusername')
+    resp.set_cookie('email', 'yangming')
+
+    return resp    
 
 
 
